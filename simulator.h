ddb13cee6b23cc31a029440cf197269eba5994bc
gerda-ugne <gerda@LAPTOP-B21F1DFU.localdomain>
2020-12-01 20:06:44 +0000

    Added fetch function along with memory allocation

--- simulator.h
+++ simulator.h
@@ -4,21 +4,28 @@
 /*ERROR CODES*/
 
 #define SUCCESS                  100
-#define INVALID_INPUT_PARAMETER  101
+#define INVALID_PARAMETER  101
 #define MEMORY_ALLOCATION_ERROR  102   
-#define INVALID_LIST_OPERATION   103
+#define INVALID_OPERATION        103
 #define FILE_IO_ERROR            104
+#define OUT_OF_BOUNDARIES        105
+#define INVALID_FETCH            106
 
 /*METHODS OF THE SIMULATOR*/
 
-/*Initializes values of the variables to default*/
-void initializeValues();
+
+/* Increments the control instruction by 1.*/
+void incrementCI();
+/* Allocates memory for the accumulator and the present instruction.*/
+int allocateMemory();
+/*  Changes the bit value for the program. */
+void changeBits (int newValue);
 /*Runs the Manchester Baby simulator*/
 void runSimulator();
 /*Increments the control instruction*/
 void incrementCI();
 /*Fetches instructions from the store*/
-void fetch();
+int fetch();
 /*Decodes the fetch instruction*/
 void decode();
 /*Displays all relevant data*/
