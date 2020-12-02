6903f99c4d0a908d99117e7c09c9d2b111dfab5b
LauraNaslenaite <l.naslenaite@dundee.ac.uk>
2020-12-02 14:51:23 +0000

    Store

--- simulator.c
+++ simulator.c
@@ -1,8 +1,7 @@
-#include <stdlib.h>
-#include <string.h> 
 #include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
 #include "simulator.h"
-
 /*Holds the default instruction set of the machine*/
 static const int instructionSet[8];
 /*Holds the adress of the current line in store for the next instruction fetch*/
@@ -16,14 +15,23 @@ static int* accumulator;
 /*Holds the opcode of the instruction to execute*/
 static int* presentInstruction;
 
-
 int main()
 {
+    
     //Void to avoid compilator complaints
-    (void)instructionSet;
+    //(void)instructionSet;
 
-    allocateMemory();
-    runSimulator(store);
+    // allocateMemory();
+    // runSimulator(store);
+
+    //Testing machine's memory with capacity of 32x32 bits
+    initialiseStore();
+    displayStore();
+    printf("\n");
+    fillStore("BabyTest1-MC.txt");
+    displayStore();
+    freeStore();
+
     return 0;
 }
 
@@ -69,17 +77,6 @@ void freeMemory()
     freeStore();
 
 }
-
-int initialiseStore()
-{
-    return 1;
-}
-
-void freeStore()
-{
-
-}
-
 /**
  * Changes the bit value for the program. 
  * Default is 32.
@@ -110,4 +107,125 @@ int fetch()
 
     return SUCCESS;
 
+}
+/*Initialises  Store to its default values 0 
+@return SUCCESS if initialization was successful otherwise MEMORY_ALLOCATION_ERROR was detected
+*/
+int initialiseStore()
+{
+    store = (int**)malloc(sizeof(int*)*bits);
+    if(store == NULL)
+        return MEMORY_ALLOCATION_ERROR;
+    for(int i =0; i<bits; i++)
+    {
+        store[i]=(int*)malloc(sizeof(int)*bits);
+        if(store[i] == NULL)
+            return MEMORY_ALLOCATION_ERROR;
+        for(int j=0; j<bits; j++)
+        {
+            store[i][j] = 0;
+        }
+    }
+    return SUCCESS;
+}
+/*Reads  a machine code from a text file and stores it in a Store
+@return SUCCESS: machine code was successfully stored in memory
+        INVALID_PARAMETER : file name was not found in the current directory
+        FILE_IO_ERROR : file was opened/closed unsuccessfully
+        INVALID_MACHINE_CODE : machine code contains invalid characters or is capacity is not compatibale with memory
+*/
+int fillStore(char fileName[] )
+{
+    if(fileName == NULL || strlen(fileName) > 50 || strlen(fileName) == 0)
+        return INVALID_PARAMETER;
+    FILE *f;
+    f = fopen(fileName, "r");
+    if(f==NULL)
+        return FILE_IO_ERROR; 
+        
+    int counter=0;
+    char line[256]; 
+    //While there is a new line and the number of lines in memory is not exceeded
+    while((fgets(line, 256,f) != NULL) && counter < bits)    
+    {
+        int len=strlen(line);
+
+        //Validation: checking if a program is not 64bits when the memory can hold only 32bits
+        if(bits == 32 && len > 60)
+        { 
+            resetStore(counter);
+            return INVALID_MACHINE_CODE;
+        }
+        
+        //Making sure that if memory is of 64 bits, a program of 32 bits could be stored 
+        int index=bits;
+        if(len < 40 && bits == 64) 
+            index =32;
+        else if(bits > 40)
+            index =64;
+
+        //Creating a duplicate array that would contain only 32 or 64 elements, since a line of characters
+        //that is read in contains some extra chars that are not needed (such as \n) 
+        char array[index];
+        for(int i =0; i<index; i++)
+        {
+            array[i] = line[i];
+        }
+    
+        for(int i =0; i<index; i++)
+        {
+            int number=array[i] - '0' ;  // Converting from char into int
+            if(number == 0 || number == 1)
+                store[counter][i] = number;
+            else
+            {
+                //Validation: if any invalid characters(like a letter or a number that is neither 1 or 0) are detected
+                //the store is reset to its default values 
+                resetStore(counter);
+                return INVALID_MACHINE_CODE;
+            }
+        }
+        counter++;
+    }
+
+    if(fclose(f) != 0)
+        return FILE_IO_ERROR;
+    return SUCCESS;
+}
+
+/*Displays current state of a memory - Store*/
+void displayStore()
+{
+    for(int i =0; i<bits; i++)
+    {
+        for(int j=0; j<bits; j++)
+        {
+            printf("%d ", store[i][j] ) ;
+        }
+        printf("\n");
+    }
+}
+/*Frees the memory of Store
+*@return SUCCESS if memory was freed successfully*/
+int freeStore()
+{
+    for (int i=0; i<bits; i++)
+    { 
+        free(store[i]); 
+    }
+    free(store);
+    return SUCCESS;
+}
+
+/* Resets Store to its default values if an error was detected in a machine code while its values were in the
+process of being stored in Store*/
+void resetStore(int row)
+{
+    for(int i = row; i >= 0; --i)
+    {
+        for(int j=0; j< bits; j++)
+        {
+            store[i][j] = 0;
+        }
+    }
 }
