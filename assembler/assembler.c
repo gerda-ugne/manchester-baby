4862c52a3ee3bb36d68f4a521e8b56264c08d15d
gerda-ugne <gerda@LAPTOP-B21F1DFU.localdomain>
2020-12-04 18:34:23 +0000

    Added starter assembler files

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include "assembler.h"
 
-int main()
+
+typedef struct instructionSet{
+char stringInstruction[10];
+char binaryInstruction[8];
+
+} InstructionSet;
+
+/**/
+InstructionSet instructions[8];
+int codeBuffer[8];
+/*Stores memory adresses of user's labels and variables*/
+int symbolTable[8];
+/*Loads and runs object code*/
+int loader;
+
+void setInstructions()
 {
-    return 0;
+
 }
---
+++ assembler/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
+#include "assembler.h"
+
+int main()
+{
+    return 0;
+}
