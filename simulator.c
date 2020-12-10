123a1f369d143fd05b72d2dbdcdf2a904a720da1
ejasaityte <55398111+ejasaityte@users.noreply.github.com>
2020-12-10 18:25:33 +0000

    Update simulator.c

--- simulator.c
+++ simulator.c
@@ -252,12 +252,12 @@ int* sumBinaryNumbers(int* binary1, int* binary2)
         if (result[i] == 2) 
         {
             result[i] = 0;
-            result[i+1] = 1;
+            if(i!=bits-1) result[i+1] = 1;
         }
         else if (result[i]==3)
         {
             result[i] = 1;
-            result[i+1] = 1;
+            if(i!=bits-1) result[i+1] = 1;
         }
     }
     return negOperand(result);
